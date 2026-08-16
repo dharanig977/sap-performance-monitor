9b6a0ad09facb755430a975e7059efce65a238d7
Dharani Gandhipathy <dharanig977@gmail.com>
2026-08-16 14:26:06 -0400

    Add memory usage monitoring

--- app.py
+++ app.py
@@ -11,3 +11,16 @@ elif CPU_usage <=80:
 
 else:
     print("SAP CPU Usage:", CPU_usage, "%", "Critical, CPU Performnace is high, action needed")
+
+memory_usage = 65
+
+print("SAP Memory Usage:", memory_usage, "%")
+
+if memory_usage <= 60:
+    print("SAP Memory Usage:", memory_usage, "%", "Memory usage is Good")
+
+elif memory_usage <= 80:
+    print("SAP Memory Usage:", memory_usage, "%", "Warning! Memory usage is slightly high")
+
+else:
+    print("SAP Memory Usage:", memory_usage, "%", "Critical! Memory usage is high, action needed")
